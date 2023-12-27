4deccfb7461e223b984fdd748269f009c600d825
david-cattermole <cattermole91@gmail.com>
2023-12-27 12:35:42 +1100

    Print database file and date/time range at start of printing.

--- print-bin/src/main.rs
+++ print-bin/src/main.rs
@@ -6,6 +6,7 @@ use clap::Parser;
 use log::{debug, warn};
 use std::time::SystemTime;
 use timetracker_core::filesystem::get_database_file_path;
+use timetracker_core::format::format_datetime;
 use timetracker_core::settings::RECORD_INTERVAL_SECONDS;
 use timetracker_core::storage::Storage;
 use timetracker_print_lib::aggregate::get_map_keys_sorted_strings;
@@ -20,6 +21,17 @@ fn print_presets(args: &CommandArguments, settings: &PrintAppSettings) -> Result
         &settings.core.database_dir,
         &settings.core.database_file_name,
     );
+    if database_file_path.is_some() {
+        println!(
+            "Database file path: {}",
+            database_file_path.as_ref().unwrap().display()
+        );
+    } else {
+        warn!(
+            "Database file {:?} not found in {:?}",
+            &settings.core.database_file_name, &settings.core.database_dir
+        );
+    }
 
     let mut storage = Storage::open_as_read_only(
         &database_file_path.expect("Database file path should be valid"),
@@ -37,6 +49,12 @@ fn print_presets(args: &CommandArguments, settings: &PrintAppSettings) -> Result
     // current week, and a value of '1' will get the next week (which
     // shouldn't really give any results, so it's probably pointless).
     let week_datetime_pair = get_relative_week_start_end(relative_week)?;
+    println!(
+        "Gathering data from {} to {}.",
+        format_datetime(week_datetime_pair.0, settings.print.format_datetime),
+        format_datetime(week_datetime_pair.1, settings.print.format_datetime),
+    );
+    println!("");
 
     let (presets, missing_preset_names) = create_presets(
         settings.print.time_scale,
